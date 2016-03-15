c6d05465539a1c45dcf1f6cc85d35255be77a1ad
zhaobaodong <190200560@qq.com>
2016-03-15 16:52:35 +0800

    暂时允许跨域

---
+++ src/main/java/org/summer/dp/cms/interceptor/ServletInterceptor.java
@@ -0,0 +1,23 @@
+package org.summer.dp.cms.interceptor;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class ServletInterceptor extends HandlerInterceptorAdapter {  
+
+    @Override  
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    	/**
+    	 * 暂时允许跨域访问
+    	 */
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");  
+        response.setHeader("Access-Control-Allow-Methods", "GET");  
+        response.setHeader("Allow", "GET");  
+        return true;  
+    }  
+
+}  
